170103173436ef6b8db865a84f2228f4a0ae7acc
shekharhsb6-max <shekharhsb6@gmail.com>
2026-08-21 16:55:14 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -50,7 +50,7 @@ import os
 
 DESTINATION_SPREADSHEET_ID = os.environ.get(
     "GOOGLE_SHEET_ID",
-    "1iMFuhNvKUpQpQoUMZaMntEhqiQpx9MOM-8Nzxgip7I8"
+    "1iMFuhNvKUpQpQoUMZaMntEhqiQpx9MOM-8NzxGip7I8"
 )
 
 NAV_SOURCE_SPREADSHEET_ID = (
